a2101ed82fc16c0368e821bb7c9ba93fbb1b8f20
LibelulaSoft Compu4 <libelulasoftcompu4@MacBook-Pro.local>
2020-08-16 18:16:43 -0500

    servicios app2

--- src/routes/cliente.routes.ts
+++ src/routes/cliente.routes.ts
@@ -5,6 +5,8 @@ import Cliente from '../controllers/ClienteController';
 const router = Router();
 
 router.get('/:id([0-9]+)/doctor/:doctor([0-9]+)', [checkJwt], Cliente.getOneById);
+
+// app
 router.put('/', Cliente.editCliente);
 // router.patch("/:id([0-9]+)", [checkJwt], DoctorController.editMedico);
 
